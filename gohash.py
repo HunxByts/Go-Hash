6485b3bd525aa4085f2c0599ff0240549c2ee50b
HunX <130666567+HunxByts@users.noreply.github.com>
2023-04-14 14:52:26 +0800

    Update gohash.py

--- gohash.py
+++ gohash.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# << CODE BY HUNXBYTS
+# << MAU RECODE ??? IZIN DULU LAH , MINIMAL TAG AKUN GITHUB MIMIN YANG MENGARAH KE AKUN INI, LEBIH ENAKNYA SIH FORK 
+# << KALAU DI ATAS TIDAK DI IKUTI MAKA AKAN MENDAPATKAN DOSA KARENA MIMIN GAK IKHLAS
+# “Wahai orang-orang yang beriman! Janganlah kamu saling memakan harta sesamamu dengan jalan yang batil,” (QS. An Nisaa': 29). Rasulullah SAW juga melarang umatnya untuk mengambil hak orang lain tanpa izin.
+
 import requests
 import json
 from sys import stderr
